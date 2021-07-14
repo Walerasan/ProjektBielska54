4b17348803b08c783e5c32fa008be6307d3511de
pzsnr1 <informatyka@pszczyna.edu.pl>
2021-07-14 19:53:37 +0200

    Aktualizacja opłaty - lista dla wszystkich uczniów dla danego opiekuna

--- pages/oplaty.php
+++ pages/oplaty.php
@@ -45,19 +45,101 @@ if(!class_exists('oplaty'))
 		private function get_payments_list($ido)
 		{
 			// pobrać wszystkich uczniów i dla nich pobrać wszystkie opłaty
-			
+			// dla danego opiekuna
 			$rettext = "";
-			//--------------------
-			$idu_array = $this->page_obj->uczniowie_opiekunowie->get_idu_list($this->page_obj->opiekunowie->get_login_ido());
-			foreach($idu_array as $idu)
-			{
-				$ido_array = $this->page_obj->uczniowie_oplaty->get_ido_list($idu);
-				foreach($ido_array as $ido)
+			$wynik = $this->page_obj->database_obj->get_data("select idu from uczniowie_opiekunowie where ido = $ido and usuniety = 'nie';");
+			if($wynik){
+				
+
+				$rettext.="
+				<style>
+					#customers {
+					font-family: Arial, Helvetica, sans-serif;
+					border-collapse: collapse;
+					width: 100%;
+					}
+
+					#customers td, #customers th {
+					border: 1px solid gray;
+					padding: 8px;
+					}
+
+					#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+					#customers tr:hover {background-color: #ddd;}
+
+					#customers th {
+					padding-top: 12px;
+					padding-bottom: 12px;
+					text-align: left;
+					background-color: orange;
+					color: white;
+					}
+					button.oplac{
+						background-color: yellow;
+						border:2px solid black;
+						border-radius:10px;
+						font-weight:bold;
+						padding:5px;
+					}
+					button.oplac:hover{
+						background-color: #2F4F4F;
+						border:2px solid black;
+						border-radius:10px;
+						font-weight:bold;
+						color:white;
+						padding:5px;
+					}
+				</style>
+				";
+				$rettext.="<table id='customers'>";
+				$rettext.="
+					<tr>
+						<th>Lp.</th>
+						<th>Imie i nazwisko</th>
+						<th>Nazwa</th>
+						<th>Kwota zł</th>
+						<th>Rabat nazwa</th>
+						<th>Rabat</th>
+						<th>Status</th>
+					</tr>";
+				$lp=1;
+
+
+
+				while(list($idu)=$wynik->fetch_row())
 				{
-					$rettext .= $idu." ".$ido." <br />";
+					$wynik_imie_nazwisko = $this->page_obj->database_obj->get_data("select imie_uczniowie, nazwisko_uczniowie from uczniowie where idu = $idu and usuniety = 'nie';");
+					list($imie,$nazwisko)=$wynik_imie_nazwisko->fetch_row();
+					
+					$wynik_uczniowie_oplaty = $this->page_obj->database_obj->get_data("select idop,rabat_kwota,rabat_nazwa,status from uczniowie_oplaty where idu = $idu and usuniety = 'nie';");
+					if($wynik_uczniowie_oplaty){
+						
+						while(list($idop,$rabat_kwota,$rabat_nazwa,$status)=$wynik_uczniowie_oplaty->fetch_row()){
+						
+							$wynik_oplaty = $this->page_obj->database_obj->get_data("select idto,nazwa,kwota from oplaty where idop = $idop and usuniety = 'nie';");
+							if($wynik_oplaty){
+								list($idto,$nazwa,$kwota)=$wynik_oplaty->fetch_row();
+								$rettext.="
+								<tr>
+									<td>$lp.</td>
+									<td>$imie $nazwisko</td>
+									<td>$nazwa</td>
+									<td>$kwota zł</td>
+									<td>$rabat_nazwa</td>
+									<td>$rabat_kwota</td>
+									<td>$status</td>
+								</tr>";
+								$lp++;
+							}
+							
+						}
+
+					}
+
 				}
+				$rettext.="</table>";
 			}
-			//--------------------
 			return $rettext;
 		}
 		#endregion
