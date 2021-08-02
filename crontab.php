7296d000599613394656429769be9250629d4367
Rafał Płatkowski <sioleskr@gmail.com>
2021-08-02 22:07:39 +0200

    Plik crontab.php z serwera

--- crontab.php
+++ crontab.php
@@ -1,44 +1,4 @@
 <?php
-	$response = file_get_contents('http://localhost/005_ProjektBielska54/ksiegowosc.nzpe.pl/wyciagi,raw,refresh');
-	$response2 = file_get_contents('http://localhost/005_ProjektBielska54/ksiegowosc.nzpe.pl/powiadomienia,raw,refresh');
-
-	$jestkatalognalogi = true;
-
-	if(!file_exists(RootPath()."/logs"))
-	{
-		if(!mkdir(RootPath()."/logs"))
-		{
-			$jestkatalognalogi = false;
-		}
-	}
-	if($jestkatalognalogi)
-	{
-		$fsize = filesize( RootPath()."/logs/crontab.log" );
-		if($fsize > 20971520)
-		{
-			unlink(RootPath()."/logs/crontab.log");
-		}
-
-		$plik=fopen(RootPath()."/logs/crontab.log",'a');
-		fwrite($plik, "//--------------------------------------------------------------------------------\n");
-		fwrite($plik, "// ".date("Y-m-d H:i:s")."\n");
-		fwrite($plik, "//--------------------------------------------------------------------------------\n");
-		fwrite($plik, $response."\n");
-		fwrite($plik, "//################################################################################\n");
-		fwrite($plik, $response2."\n");
-		fwrite($plik, "//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
-		fclose($plik);
-	}
-
-	function RootPath()
-	{
-		$path_full = dirname($_SERVER['PHP_SELF']);
-		$path_tab = explode("/", $path_full);
-		$path_count = count($path_tab);
-		$path="";
-		for($i=2;$i<count($path_tab);$i++)
-			$path.="../";
-		if($path=="")$path="./";
-		return $path;
-	}
+	$response = file_get_contents('http://ksiegowosc.nzpe.pl/wyciagi,raw,refresh');
+	$response2 = file_get_contents('http://ksiegowosc.nzpe.pl/powiadomienia,raw,refresh');
 ?>
